c02c19db87757fd7d6fcdaba2aa0b1230a320054
bongminkim <klbm126@gmail.com>
2019-10-17 10:10:06 +0900

    1016

--- transformers/TFIDF.py
+++ transformers/TFIDF.py
@@ -16,7 +16,7 @@ def get_TFIDF_Result(title_idx, cosine_sim, indices, data_Des):
     idx = int(idx)
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:31]
+    sim_scores = sim_scores[1:11]
     data_indices = [i[0] for i in sim_scores]
     num=1
     print("\n")
@@ -47,15 +47,22 @@ def main__run():
     data_num = []
     data_Name = []
     data_Des = []
+    num = 0
+    name = 0
     k = 0
 
     f = open('bad_long_des_after_bert.tsv', 'r', encoding='utf-8-sig')
     rdr = csv.reader(f, delimiter='\t')
     for line in rdr:
-        data_num.append(line[0])
-        data_Name.append(line[1])
-        data_Des.append(line[2])
-        k += 1
+        if len(line[2]) > 100:
+            #data_num.append(line[0])
+            #data_Name.append(line[1])
+            data_num.append(num)
+            data_Name.append(name)
+            data_Des.append(line[2])
+            num+=1
+            name+=1
+            #k += 1
     f.close()
 
     pd_data_des = pd.Series(data_Des, index=data_num)
--- transformers/bring_zeroLabel_des.py
+++ transformers/bring_zeroLabel_des.py
@@ -25,17 +25,30 @@ def bring_test_file(file, count):
     f.close()
 
 def print_bad_des(file):
+    count = 0
     max = 0
     sum = 0
     num = 10
-    f = open("./GLUE_DIR/MRPC/%s" % file, 'r', encoding="utf-8")
+    f = open("./GLUE_DIR/MRPC/%s" % file, 'r', encoding="utf-8") # 여기를 애초에 만든 bad des 를 넣어줘야함.
     rdr = csv.reader(f, delimiter='\t')
     for line in rdr:
+        if count == 9 :
+            break
         if line[0] in zero_des:
             sum += len(line[1])
             if max < len(line[1]):
                 max = len(line[1])
-            if len(line[1]) > 70:
-                print("index : ", line[0], ", shot_des : ", line[1])
+            if len(line[1]) > 90:
+                print("index : ", line[0], "\nshot_des : ", line[1])
+                f2 = open('./GLUE_DIR/MRPC/ld1_allp.tsv','r',encoding=
+                          'utf-8-sig')
+                rdr2 = csv.reader(f2, delimiter='\t')
+                for line2 in rdr2:
+                    if line[0] == line2[0]:
+                        print("\nlong_des : ",line2[2],"\n")
+                        print("------------------------------------------"
+                              "---------------------------------------")
+                        break
+                count += 1
                 index.append(line[0])
     f.close()
